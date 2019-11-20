0e4dfb487536dbc3da1ce5ef9f1bd162ae5727cc
Sam Flavin <flavi022@d.umn.edu>
2019-11-20 14:26:00 -0600

    multiexecutor

---
+++ MultiExecutor/src/MultiExecutor.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class MultiExecutor {
+
+    // Add any necessary member variables here
+
+    /*
+     * @param tasks to executed concurrently
+     */
+    public MultiExecutor(List<Runnable> tasks) {
+        // Complete your code here
+    }
+
+    /**
+     * Starts and executes all the tasks concurrently
+     */
+    public void executeAll() {
+        // complete your code here
+    }
+}
